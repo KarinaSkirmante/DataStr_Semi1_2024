62b86106293abf4fb5546232d18ef89faf55c963
localuser <localuser@VEA.LV>
2024-03-13 10:06:15 +0200

    Add: test in MainService

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -3,6 +3,7 @@ package service;
 import java.util.Arrays;
 
 import datastr.MyArrayList;
+import model.Student;
 
 
 //TODO nokopēt Student klasi un ielikt model pakotnē
@@ -41,6 +42,48 @@ public class MainService {
 		}
 		
 
+		System.out.println("-----------------------------");
+		MyArrayList<Student> allStudents = new MyArrayList<Student>();
+		
+		Student s1 = new Student("Janis", "Berzins");
+		Student s2 = new Student("Liga", "Jauka");
+		Student s3 = new Student("Baiba", "Kalnina");
+		
+		allStudents.add(s1);//Janis
+		allStudents.add(s2);//Janis Liga
+		try
+		{
+			allStudents.add(0, s3); //Baiba Janis Liga
+			allStudents.print();//Baiba Janis Liga
+			
+			System.out.println(allStudents.search(s1));//1
+			System.out.println(Arrays.toString(allStudents.getNeighbours(s1)));//Liga
+			
+			allStudents.print();//Baiba Janis Liga
+			//allStudents.sort();
+			allStudents.print();//Baiba Janis Liga
+				
+			
+			allStudents.remove(1);
+			allStudents.print();//Baiba Liga
+			
+			allStudents.makeEmpty();
+			allStudents.add(s1);
+			allStudents.print();//Janis
+			
+		
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
